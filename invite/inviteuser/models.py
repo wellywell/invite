798b4606136a7c8605b5a235108b2d73b2a2add3
anna <anna@bodhivm.(none)>
2016-06-11 20:27:26 +0300

    Creating models and tests for those

--- invite/inviteuser/models.py
+++ invite/inviteuser/models.py
@@ -1,5 +1,60 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.core.mail import send_mail
 from django.db import models
+from django.contrib.auth.models import User
+import random, string
 
-# Create your models here.
+
+class DoubleInviteException(Exception):
+    pass
+
+
+password  = lambda : ''.join(random.choice(string.letters + string.digits) for _ in range(10))
+
+
+
+
+class Invite(models.Model):
+
+    #random identifier
+    value = models.IntegerField(unique=True)
+    email = models.EmailField(unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+
+    @classmethod
+    def create(cls, email):
+        random.seed()
+
+        #check for collisions
+	#Use random numbers so that user knows nothing about their real id in database
+        while True:
+	    val = random.randint(1, 2147483647)
+            invs = Invite.objects.filter(value=val)
+	    if len(invs) == 0:
+		 break
+        invite = cls(value=val, email=email)
+	invite.save()
+        send_mail(u'Инвайт для регистрации на сайте', u'Поздравляю! Вы получили инвайт! Для регистрации на сайте перейдите по ссылке http://localhost:8000/%s' % invite.value, 'noreply@inviter.com', [invite.email], fail_silently=False)
+
+        return invite
+
+    @classmethod
+    def create_user(cls, invite):
+	if invite.user != None:
+            raise DoubleInviteException("This Invite has been used! The user already exists.")
+
+        passw = password()
+        uname = "user#" + str(invite.id * 42)       
+
+        send_mail(u'Данные для входа на сайт',
+        u'Вы зарегестрированы на сайте. \nЛогин: %s \nПароль: %s' % (uname, passw),
+        'noreply@inviter.com',
+        [invite.email],
+        fail_silently=False)
+
+
+	user = User.objects.create_user(username=uname, email=invite.email, password=passw)
+	invite.user = user
+	invite.save()
+	return user
--- invite/inviteuser/tests.py
+++ invite/inviteuser/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from inviteuser.models import Invite, DoubleInviteException
+from django.contrib.auth.models import User
+from django.db import IntegrityError #, DoesNotExist
+from django.core.mail import send_mail
+from django.core import mail
 
-# Create your tests here.
+class InvitedUserTestCase(TestCase):
+
+    def  setUp(self):
+        Invite.create(email="testinvite@gmail.com")
+
+
+    def test_email(self):
+        #just must not fail
+        send_mail("Test", "Test text", "test@test.test", ["test2@test.test"], fail_silently=False) 
+        
+
+    def test_create_user(self):
+        print "****************************************\n\nTesting create user"	
+        invite = Invite.objects.get(email="testinvite@gmail.com")
+        user = Invite.create_user(invite)
+        self.assertEqual(invite.user, user)
+        self.assertEqual(invite.email, user.email)
+	#sent invite
+	print mail.outbox[0].body
+	#sent credentials
+	print mail.outbox[1].body
+	
+
+    def test_no_double_used_invites(self):
+        print "****************************************\n\nTest can't use the same invite twice"
+	
+	invite = Invite.objects.create(email="ololo@ololo.lo", value=-1)
+	Invite.create_user(invite)
+	self.assertRaises(DoubleInviteException, Invite.create_user, invite)
+
+
+
+    def test_del(self):
+	print "******************************************\n\nTest invite is terminated on deletion of user"
+
+	inv = Invite.create(email="mmmm@mail.com")
+	user = User.objects.create_user(username="user", email="mmmm@mail.com", password="123")
+        
+	inv.user = user
+	inv.save()
+        user.delete()
+	#Invite.objects.get(email="mmm@mail.com")
+        self.assertRaises(Invite.DoesNotExist, Invite.objects.get, email="mmm@mail.com")
+         
